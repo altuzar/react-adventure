0f21b625cd24907bf0c40f39223c7ea5174f2f73
Fernando Altuzar <fernando.altuzar@shopify.com>
2020-03-27 12:30:27 -0400

    Add todos app

---
+++ redux/todos_app/src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import { connect } from 'react-redux'
+import {
+  handleInitialData
+} from '../actions/shared'
+
+class App extends React.Component {
+  componentDidMount () {
+    const { dispatch } = this.props
+
+    dispatch(handleInitialData())
+  }
+  render() {
+    const { loading } = this.props
+
+    if (loading === true) {
+      return <h3>Loading</h3>
+    }
+
+    return (
+      <div>
+        <ConnectedTodos />
+        <ConnectedGoals />
+      </div>
+    )
+  }
+}
+
+export default connect((state) => ({
+  loading: state.loading
+}))(App)
